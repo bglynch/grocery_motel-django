081f8fe365e285e09b4250941460a29338491eb9
Brian Lynch <bglynch17@gmail.com>
2018-11-19 18:39:54 +0000

    Script 'b': deleted lines in settings.py, added user feedback

--- scripts/b-media-and-static.sh
+++ scripts/b-media-and-static.sh
@@ -12,26 +12,44 @@ mkdir static/images
 touch static/css/style.css
 touch static/js/custom.js
 
+
 # MEDIA
 # create media folder and add it to .gitignore file
 mkdir media
 echo media >> .gitignore
 
+
+# SETTINGS
+# delete unnecessary lines form settings
+sed -i -e '118,119d' $DJANGO_PROJECT/settings.py
+sed -i -e '104,105d' $DJANGO_PROJECT/settings.py
+sed -i -e '85,86d' $DJANGO_PROJECT/settings.py
+sed -i -e '74,75d' $DJANGO_PROJECT/settings.py
+sed -i -e '18,21d' $DJANGO_PROJECT/settings.py
+sed -i -e '1,12d' $DJANGO_PROJECT/settings.py
+
+
 # add media context processor to settings
 sed -i -e "/'django.contrib.messages.context_processors.messages',/a\                'django.template.context_processors.media'," ./$DJANGO_PROJECT/settings.py
 
+# add message storage to settings
+echo >> ./$DJANGO_PROJECT/settings.py
+echo >> ./$DJANGO_PROJECT/settings.py
+echo "# Message storage for flash messages" >> ./$DJANGO_PROJECT/settings.py
+echo -e "MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'" >> ./$DJANGO_PROJECT/settings.py
+
+
+# INSTALLATIONS
 # install pillow, Python Imaging Library
 pip install Pillow
 pip freeze > requirements.txt
 
-# add message storage to settings
-echo -e "\n MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'" >> ./$DJANGO_PROJECT/settings.py 
 
-# ======================================== #
-# if you are not splitting up your settings.py file into production and local
-# uncomment lines 36 and 37
-# ======================================== #
-# add media root and message storage to settings
-# add media roots to settings.py
-# echo MEDIA_URL = '/media/' >> ./$DJANGO_PROJECT/settings.py 
-# echo MEDIA_ROOT = os.path.join\(BASE_DIR, 'media'\) >> ./$DJANGO_PROJECT/settings.py 
+# USER FEEDBACK
+echo
+echo -e "\e[1;36m/---------- Static and Media script ----------/\e[0m"
+echo "Installed: Pillow"
+echo "Created static and media directories"
+echo "added media context processor"
+echo "added message storage for flash messages"
+echo
