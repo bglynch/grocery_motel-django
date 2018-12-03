fee1963d114b52a978f0ff002ea60ed98e30beb6
Brian Lynch <bglynch17@gmail.com>
2018-12-03 23:13:08 +0000

    user registration functionality added

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegisterForm
 
-# Create your views here.
+
+# function to register a new user
+def register(request):
+    # submitting a register form
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, 'Account created for {}!'.format(username))
+            return redirect('home')
+    # show blank register form
+    else:
+        form = UserRegisterForm()
+    return render(request, 'accounts/register.html', {'form': form})
--- grocerymotel/urls.py
+++ grocerymotel/urls.py
@@ -8,10 +8,12 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from home.views import get_home
+from accounts import views as accounts_views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', accounts_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='accounts/logout.html'), name='logout'),    
     path('', get_home, name="home"),
