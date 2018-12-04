faa1c7b9489554d4f453e42dcbdda3370e1f67c3
Brian Lynch <bglynch17@gmail.com>
2018-12-04 13:45:32 +0000

    Testing: added testing for accounts/views.py, added passing badge to README

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Grocery Motel - Online Grocery Shopping
+[![Build Status](https://travis-ci.org/bglynch/grocery_motel-django.svg?branch=master)](https://travis-ci.org/bglynch/grocery_motel-django)
 
 ## Summary
 Grocery Motel is a web app dedicated to provided customers with weekly groceries consisting of fresh, in-season produce.
--- accounts/tests.py
+++ accounts/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.apps import apps
+from django.contrib.auth.models import User
 from .forms import UserRegisterForm
 from .apps import AccountsConfig
 
@@ -7,8 +8,7 @@ from .apps import AccountsConfig
 class TestAccountsForms(TestCase):
     '''Testing the forms.py file'''
 
-    # ---------------------------------------------------Sad Path Tests
-
+    # ---------------------------------------------------Happy Path Tests
     def test_register(self):
         form = UserRegisterForm({
             'username': 'benji',
@@ -20,7 +20,6 @@ class TestAccountsForms(TestCase):
 
 
     # ---------------------------------------------------Sad Path Tests
-
     def test_register_poor_password(self):
         form = UserRegisterForm({
             'username': 'benji',
@@ -60,11 +59,53 @@ class TestAccountsForms(TestCase):
             )
 
 
-# class TestAccountsViews(TestCase):
+class TestAccountsViews(TestCase):
+    '''Testing the views.py file'''
+    
+    # ------------------------------------------def register(request)
+    def test_get_register_page(self):
+        # Create and login a user
+        User.objects.create_user(username="benji", email="benji@ex.com", password="h3!!oPass")
+        self.client.login(username='benji', password='h3!!oPass')
+        
+        page = self.client.get("/register/")
+        
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "accounts/register.html")
+        
+    
+    def test_register_a_new_user(self):
+        
+        response = self.client.post("/register/", {
+            'username': 'benji',
+            'email': 'benji@ex.com',
+            'password1': 'h3!!oPass',
+            'password2': 'h3!!oPass'
+        })
+        self.assertRedirects(
+            response,
+            '/',
+            status_code=302,
+            target_status_code=200,
+            fetch_redirect_response=True
+            )        
+
+    
+    # ------------------------------------------def my_account(request):
+    def test_get_profile_page(self):
+        # Create and login a user
+        User.objects.create_user(username="benji", email="benji@ex.com", password="h3!!oPass")
+        self.client.login(username='benji', password='h3!!oPass')
+        
+        page = self.client.get("/my_account/")
+        
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "accounts/my-account.html")
 
 
 class TestAccountsApps(TestCase):
     '''Testing the apps.py file'''    
+    
     def test_apps(self):
         self.assertEqual(AccountsConfig.name, 'accounts')
         self.assertEqual(apps.get_app_config('accounts').name, 'accounts')
